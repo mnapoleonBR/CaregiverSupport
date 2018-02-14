5653347caf1843922b9fd80d8891871d1376e091
Tyler Hakomori <tyler_hakomori@brown.edu>
2018-02-13 23:23:48 -0500

    added support for heroku

--- webapp/main.py
+++ webapp/main.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
