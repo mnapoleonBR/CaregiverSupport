7452785bd72eb3b2f1d5a334662d7dca87d37874
Iamjaredlee <jared_lee@brown.edu>
2018-02-26 13:03:22 -0500

    GCal API Integration

--- webapp/static/assets/js/calendar.js
+++ webapp/static/assets/js/calendar.js
@@ -1,15 +1,12 @@
-
-
-
 $(document).ready(function() {
-
     // page is now ready, initialize the calendar...
     console.log("here?")
     $('#calendar').fullCalendar({
         // put your options and callbacks here
-        defaultDate: '2014-09-12',
-        editable: true,
-        eventLimit: true
+   		googleCalendarApiKey: 'AIzaSyDgGyzwt9WQjT9bDBZu4PXxnJuHSQLtHdY',
+   		events: {
+   			googleCalendarId: 'jh4k25spig0mj610ipq0de83h0@group.calendar.google.com'
+   		}
     })
 
 });
