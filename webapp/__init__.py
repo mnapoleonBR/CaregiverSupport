92fd48fda287e41a985526fbe93226daa2318393
Iamjaredlee <jared_lee@brown.edu>
2018-02-26 22:49:09 -0500

    Styling of calendar and hover extra detail

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return template('home', names=['Chelse', 'Elaine', 'Jared', 'Tyler', 'Valeria'])
+
+@app.route('/calendar')
+def calendar():
+    return template('calendar')
+
--- webapp/static/assets/js/calendar.js
+++ webapp/static/assets/js/calendar.js
@@ -1,12 +1,56 @@
 $(document).ready(function() {
     // page is now ready, initialize the calendar...
-    console.log("here?")
     $('#calendar').fullCalendar({
         // put your options and callbacks here
    		googleCalendarApiKey: 'AIzaSyDgGyzwt9WQjT9bDBZu4PXxnJuHSQLtHdY',
    		events: {
    			googleCalendarId: 'jh4k25spig0mj610ipq0de83h0@group.calendar.google.com'
-   		}
+   		},   		
+   		header: {
+   			left: 'title, today',
+   			right: 'month,agendaWeek,listMonth, prev,next'
+   		}, 
+   		eventColor: '#ca6015', 
+
+
+   		// Adds event description from GCal to date object for use in eventMouseover
+   		eventRender: function(event, element) {
+   			element.description = event.description,
+   			element.location = event.location
+   		}, 
+
+
+
+   		eventMouseover: function(date, jsEvent, view) {
+	        mouseOverFill(date)
+	    }
+
     })
 
 });
+
+
+function mouseOverFill(event) {
+	var start = event.start.format('MMMM Do YYYY, h:mm a')
+	var end = event.end.format('MMMM Do YYYY, h:mm a')
+
+	$('#title').empty()
+	$('#location').empty()
+	$('#description').empty()
+	$('#dateTime').empty()
+
+	$('<a href=' + event.url + '>' + event.title + '</a>').appendTo('#title')
+
+	if(typeof event.location !== "undefined") {
+		$('<p> ' + event.location + ' </p>').appendTo('#location');
+	} 
+
+	if(typeof event.description !== "undefined") {
+		$('<p> ' + event.description + ' </p>').appendTo('#description');
+	} 
+
+	if(typeof event.start !== "undefined" && event.end !== "undefined") {
+		$('<p> ' + start + ' - ' + end + ' </p>').appendTo('#dateTime');
+	} 
+
+} 
