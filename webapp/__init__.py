86b8ca437894d223ff0d1326461ec7567214059b
Tyler Hakomori <tyler_hakomori@brown.edu>
2018-04-08 16:19:34 -0400

    took questions out of the route file

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,35 +1,9 @@
 from flask import Flask, abort
 from webapp.helpers import template
 from webapp.resources_list import resources_list
-import os
+from webapp.survey_questions import questions
 
 app = Flask(__name__)
-
-questions = [
-    {
-        "description": "What is your favorite color?",
-        "options": [
-            {"val": "red", "text": "Red"},
-            {"val": "blue", "text": "Blue"},
-            {"val": "green", "text": "Green"}
-        ]
-    },
-    {
-        "description": "What is your favorite Japanese food?",
-        "options": [
-            {"val": "ramen", "text": "Ramen, duh."},
-            {"val": "udon", "text": "Udon!"},
-            {"val": "sukiyaki", "text": "Sukiyaki!"}
-        ]
-    },
-    {
-        "description": "Which is better, Ratty or Vdub??",
-        "options": [
-            {"val": "ratty", "text": "Ratty"},
-            {"val": "vdub", "text": "Vdub"}
-        ]
-    }
-]
 
 @app.route('/')
 def home():
