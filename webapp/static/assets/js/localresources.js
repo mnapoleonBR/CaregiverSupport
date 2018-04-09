bf958c5a7d10df8e387f3b16e5ac61becdd908df
Tyler Hakomori <tyler_hakomori@brown.edu>
2018-04-08 21:06:17 -0400

    rewrote the local resources page in jquery

---
+++ webapp/static/assets/js/localresources.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  var keywordMap = {
+    "Keyword 1": ["respite", "stress"],
+    "Keyword 2": ["long-term", "dementia"],
+  };
+
+  // need to hold onto the resource references since they will be detached from DOM
+  var $allResources = $(".resultColumn");
+  var $resourcesContainer = $(".resourceResults");
+
+  // Setting up the autocomplete
+  var input = document.getElementById("resourceSearchBar");
+  new Awesomplete(input, {
+    list: ["Keyword 1", "Keyword 2"],
+    autoFirst: true,
+    minChars: 1,
+    filter: Awesomplete.FILTER_STARTSWITH,
+  });
+
+  Awesomplete.$.bind(input, {
+    "awesomplete-selectcomplete": function(evt) {
+      showRelevantResource(evt.text);
+    }
+  });
+
+  function showRelevantResource(selectedKeyword) {
+    var relevantIds = keywordMap[selectedKeyword];
+    var $relevantResources = $(); 
+    relevantIds.forEach(function(id) {
+      var $resource = $allResources.filter("#" + id)
+      $relevantResources = $relevantResources.add($resource);
+    })
+
+    $allResources.detach();
+    $relevantResources.appendTo($resourcesContainer);
+  }
+
+  // when the search bar is back to empty, show all
+  $("#resourceSearchBar").on("input", function(e) {
+    if (!e.target.value) {
+      $allResources.detach();
+      $allResources.appendTo($resourcesContainer);
+    }
+  });
+
+  $(".resultPanel").click(function(e) {
+    resourceName = e.target.id;
+    // console.log(resourceName)
+    window.location.href = "/resource/" + resourceName;
+  });
+});
