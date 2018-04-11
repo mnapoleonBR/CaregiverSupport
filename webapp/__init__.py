ae5966a390e3576fe5024e714644e2a9675cee94
Iamjaredlee <jared_lee@brown.edu>
2018-04-11 16:16:14 -0400

    Calendar stuff

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,34 @@
 from flask import Flask
+from flask import request
+import flask
 from webapp.helpers import template
 import os
+import requests
+from datetime import datetime
+
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
+import emailUtils
+
+# This variable specifies the name of a file that contains the OAuth 2.0
+# information for this application, including its client_id and client_secret.
+CLIENT_SECRETS_FILE = "client_secret.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly', 'https://www.googleapis.com/auth/calendar']
+API_SERVICE_NAME = 'calendar'
+API_VERSION = 'v3'
+
+# Allows for http for oath2, should be changed at some point
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__)
+
+app.secret_key = 'CHANGE THIS EVENTUALLY'
 
 questions = [
     {
@@ -57,4 +83,149 @@ def respite():
 @app.route('/long-term')
 def long_term():
     return template('long_term_care')
+
+# Send email event to BakerRipley employee for approval
+@app.route('/submitEvent', methods=['POST'])
+def submitEvent():
+    msg = emailUtils.createEventEmail(request)
+    emailUtils.send_email(msg)
+    return 'success'
+
+# Add event to BakerRipley Google Calendar
+@app.route('/createEvent')
+def createEvent():
+    CALENDAR_ID = 'jh4k25spig0mj610ipq0de83h0@group.calendar.google.com'
+    timezone = 'America/Chicago'
+    dateFormat = "%m/%d/%Y %I:%M %p"
+
+    if 'event-name' not in flask.session:
+        flask.session['event-name'] = request.args.get('event-name')
+        flask.session['event-location'] = request.args.get('event-location')
+        flask.session['event-description'] = request.args.get('event-description')
+        flask.session['start-date'] = request.args.get('start-date')
+        flask.session['end-date'] = request.args.get('end-date')
+
+    summary = flask.session['event-name'] 
+    location = flask.session['event-location'] 
+    description = flask.session['event-description'] 
+    startDate = flask.session['start-date'] 
+    endDate = flask.session['end-date'] 
+
+    startDateFormatted = datetime.strptime(startDate, dateFormat).isoformat('T')
+    endDateFormatted= datetime.strptime(endDate, dateFormat).isoformat('T')
+
+    event = {
+      'summary': summary,
+      'location': location,
+      'description': description,
+      'start': {
+        'dateTime': startDateFormatted,
+        'timeZone': timezone,
+      },
+      'end': {
+        'dateTime': endDateFormatted,
+        'timeZone': timezone,
+      }
+    }
+
+    if 'credentials' not in flask.session:
+        return flask.redirect('authorize', code=307)
+
+    # Load credentials from the session.
+    credentials = google.oauth2.credentials.Credentials(
+      **flask.session['credentials'])
+
+    service = googleapiclient.discovery.build(
+      API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+    event = service.events().insert(calendarId=CALENDAR_ID, body=event).execute()
+
+    print event
+
+    # Save credentials back to session in case access token was refreshed.
+    # ACTION ITEM: In a production app, you likely want to save these
+    #              credentials in a persistent database instead.
+    flask.session['credentials'] = credentials_to_dict(credentials)
+
+    flask.session.pop('event-name')
+    flask.session.pop('event-location')
+    flask.session.pop('event-description')
+    flask.session.pop('start-date')
+    flask.session.pop('end-date')
+
+    return 'success'
+
+
+@app.route('/authorize')
+def authorize():
+  # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE, scopes=SCOPES)
+
+  flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+  authorization_url, state = flow.authorization_url(
+      # Enable offline access so that you can refresh an access token without
+      # re-prompting the user for permission. Recommended for web server apps.
+      access_type='offline',
+      # Enable incremental authorization. Recommended as a best practice.
+      include_granted_scopes='true')
+
+  # Store the state so the callback can verify the auth server response.
+  flask.session['state'] = state
+
+  return flask.redirect(authorization_url, code=307)
+
+
+@app.route('/oauth2callback')
+def oauth2callback():
+  # Specify the state when creating the flow in the callback so that it can
+  # verified in the authorization server response.
+  state = flask.session['state']
+
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+  flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+  # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+  authorization_response = flask.request.url
+  flow.fetch_token(authorization_response=authorization_response)
+
+  # Store credentials in the session.
+  # ACTION ITEM: In a production app, you likely want to save these
+  #              credentials in a persistent database instead.
+  credentials = flow.credentials
+  flask.session['credentials'] = credentials_to_dict(credentials)
+
+  return flask.redirect(flask.url_for('createEvent'), code=307)
+
+
+def credentials_to_dict(credentials):
+  return {'token': credentials.token,
+          'refresh_token': credentials.refresh_token,
+          'token_uri': credentials.token_uri,
+          'client_id': credentials.client_id,
+          'client_secret': credentials.client_secret,
+          'scopes': credentials.scopes}
+
+def print_index_table():
+  return ('<table>' +
+          '<tr><td><a href="/test">Test an API request</a></td>' +
+          '<td>Submit an API request and see a formatted JSON response. ' +
+          '    Go through the authorization flow if there are no stored ' +
+          '    credentials for the user.</td></tr>' +
+          '<tr><td><a href="/authorize">Test the auth flow directly</a></td>' +
+          '<td>Go directly to the authorization flow. If there are stored ' +
+          '    credentials, you still might not be prompted to reauthorize ' +
+          '    the application.</td></tr>' +
+          '<tr><td><a href="/revoke">Revoke current credentials</a></td>' +
+          '<td>Revoke the access token associated with the current user ' +
+          '    session. After revoking credentials, if you go to the test ' +
+          '    page, you should see an <code>invalid_grant</code> error.' +
+          '</td></tr>' +
+          '<tr><td><a href="/clear">Clear Flask session credentials</a></td>' +
+          '<td>Clear the access token currently stored in the user session. ' +
+          '    After clearing the token, if you <a href="/test">test the ' +
+          '    API request</a> again, you should go back to the auth flow.' +
+          '</td></tr></table>')
 
--- webapp/static/assets/js/calendar.js
+++ webapp/static/assets/js/calendar.js
@@ -1,34 +1,71 @@
 $(document).ready(function() {
-    // page is now ready, initialize the calendar...
-    $('#calendar').fullCalendar({
-        // put your options and callbacks here
-   		googleCalendarApiKey: 'AIzaSyDgGyzwt9WQjT9bDBZu4PXxnJuHSQLtHdY',
-   		events: {
-   			googleCalendarId: 'jh4k25spig0mj610ipq0de83h0@group.calendar.google.com'
-   		},   		
-   		header: {
-   			left: 'title, today',
-   			right: 'month,agendaWeek,listMonth, prev,next'
-   		}, 
-   		eventColor: '#ca6015', 
+	// page is now ready, initialize the calendar...
+	var API_KEY = 'AIzaSyDgGyzwt9WQjT9bDBZu4PXxnJuHSQLtHdY'
+	var CALENDAR_ID = 'jh4k25spig0mj610ipq0de83h0@group.calendar.google.com'
+	$('#calendar').fullCalendar({
+	// put your options and callbacks here
+		googleCalendarApiKey: API_KEY,
+		events: {
+			googleCalendarId: CALENDAR_ID
+		},   		
+		header: {
+			left: 'title, today',
+			right: 'month,agendaWeek,listMonth, prev,next'
+		}, 
+		eventColor: '#ca6015', 
 
+		// Adds event description from GCal to date object for use in eventMouseover
+		eventRender: function(event, element) {
+			element.description = event.description,
+			element.location = event.location
+		}, 
 
-   		// Adds event description from GCal to date object for use in eventMouseover
-   		eventRender: function(event, element) {
-   			element.description = event.description,
-   			element.location = event.location
-   		}, 
+		eventMouseover: function(date, jsEvent, view) {
+		mouseOverFill(date)
+	}
+})
 
+	$('#eventForm').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			url:'/submitEvent',
+			type:'post',
+			data:$('#eventForm').serialize(),
+			success:function(){
+				cleanModal();
+				$('#modalBody').append("<p> Event submitted successfully! A BakerRipley caregiving specialist will review your suggestion shortly. </p>");
+			},
+			error:function(){
+				cleanModal();
+				$('#modalBody').append("<p> An error occurred, please refresh the page and try again. </p>");
+			}
+		});
+	});
 
+	$('#datetimepicker1').datetimepicker({
+            icons: {
+                time: "fa fa-clock-o",
+                date: "fa fa-calendar",
+                up: "fa fa-arrow-up",
+                down: "fa fa-arrow-down"
+            }
+    	});
 
-   		eventMouseover: function(date, jsEvent, view) {
-	        mouseOverFill(date)
-	    }
-
-    })
+	$('#datetimepicker2').datetimepicker({
+        icons: {
+            time: "fa fa-clock-o",
+            date: "fa fa-calendar",
+            up: "fa fa-arrow-up",
+            down: "fa fa-arrow-down"
+        }
+	});
 
 });
 
+function cleanModal() {
+	$('#modalBody').empty();
+	$('#submit').hide();
+}
 
 function mouseOverFill(event) {
 	var start = event.start.format('MMMM Do YYYY, h:mm a')
